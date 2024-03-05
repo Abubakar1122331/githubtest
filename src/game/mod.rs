92e7425e8b970e76db4e95b873e1539d25df3048
Abubakar1122331 <abubakar493.pro@gmail.com>
2024-03-05 19:31:47 +0500

    First Commit!!!!!!
    This is my first git commit
    Commiting Bevy-Ball-Game
    Added Gitignore

---
+++ src/game/mod.rs
@@ -0,0 +1,44 @@
+pub mod enemy;
+mod player;
+pub mod score;
+pub mod star;
+mod systems;
+
+use crate::AppState;
+use enemy::EnemyPlugin;
+use player::PlayerPlugin;
+use score::ScorePlugin;
+use star::StarPlugin;
+use systems::*;
+
+use crate::events::GameOver;
+
+use bevy::prelude::*;
+
+pub struct GamePlugin;
+
+impl Plugin for GamePlugin {
+    fn build(&self, app: &mut App) {
+        app.add_state::<SimulationState>()
+            // Events
+            .add_event::<GameOver>()
+            // On Enter Systems
+            .add_system(pause_simulation.in_schedule(OnEnter(AppState::Game)))
+            // Plugins
+            .add_plugin(EnemyPlugin)
+            .add_plugin(PlayerPlugin)
+            .add_plugin(ScorePlugin)
+            .add_plugin(StarPlugin)
+            // Systems
+            .add_system(toggle_simulation.run_if(in_state(AppState::Game)))
+            // On Exit System
+            .add_system(resume_simulation.in_schedule(OnExit(AppState::Game)));
+    }
+}
+
+#[derive(States, Debug, Clone, Copy, Eq, PartialEq, Hash, Default)]
+pub enum SimulationState {
+    #[default]
+    Running,
+    Paused,
+}
---
+++ src/systems.rs
@@ -0,0 +1,61 @@
+use bevy::app::AppExit;
+use bevy::prelude::*;
+use bevy::window::PrimaryWindow;
+
+use crate::events::*;
+use crate::game::SimulationState;
+use crate::AppState;
+
+pub fn spawn_camera(mut commands: Commands, window_query: Query<&Window, With<PrimaryWindow>>) {
+    let window = window_query.get_single().unwrap();
+
+    commands.spawn(Camera2dBundle {
+        transform: Transform::from_xyz(window.width() / 2.0, window.height() / 2.0, 0.0),
+        ..default()
+    });
+}
+
+pub fn transition_to_game_state(
+    keyboard_input: Res<Input<KeyCode>>,
+    app_state: Res<State<AppState>>,
+    mut next_app_state: ResMut<NextState<AppState>>,
+) {
+    if keyboard_input.just_pressed(KeyCode::G) {
+        if app_state.0 != AppState::Game {
+            next_app_state.set(AppState::Game);
+            println!("Enetered AppState::Game");
+        }
+    }
+}
+
+pub fn transition_to_main_menu_state(
+    mut next_app_state: ResMut<NextState<AppState>>,
+    keyboard_input: Res<Input<KeyCode>>,
+    app_state: Res<State<AppState>>,
+) {
+    if keyboard_input.just_pressed(KeyCode::M) {
+        if app_state.0 != AppState::MainMenu {
+            next_app_state.set(AppState::MainMenu);
+            println!("Enetered AppState::MainMenu");
+        }
+    }
+}
+
+pub fn exit_game(
+    keyboard_input: Res<Input<KeyCode>>,
+    mut app_exit_event_writer: EventWriter<AppExit>,
+) {
+    if keyboard_input.just_pressed(KeyCode::Escape) {
+        app_exit_event_writer.send(AppExit);
+    }
+}
+
+pub fn handle_game_over(
+    mut next_app_state: ResMut<NextState<AppState>>,
+    mut game_over_event_reader: EventReader<GameOver>,
+) {
+    for event in game_over_event_reader.iter() {
+        println!("Your final score is {}", event.score.to_string());
+        next_app_state.set(AppState::GameOver);
+    }
+}
